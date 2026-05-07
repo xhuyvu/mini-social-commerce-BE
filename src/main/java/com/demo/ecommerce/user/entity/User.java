8d984b8c1a41bc39acffb499c13d6fad5cdc01f9
xhuyvu <xhuyvu.eng@gmail.com>
2026-05-08 03:46:04 +0700

    feat: change some thing here

--- src/main/java/com/demo/ecommerce/user/entity/User.java
+++ src/main/java/com/demo/ecommerce/user/entity/User.java
@@ -2,59 +2,58 @@ package com.demo.ecommerce.user.entity;
 
 import jakarta.persistence.*;
 
-@Entity(name = "users")
+@Entity
 @Table(name = "users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private String username;
-    private String email;
-    private String password;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
 
-    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
-    private Profile profile;
+  private String username;
+  private String email;
+  private String password;
 
-    protected User() {
+  @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+  private Profile profile;
+
+  protected User() {}
+
+  public long getId() {
+    return id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Profile getProfile() {
+    return profile;
+  }
+
+  public void setProfile(Profile profile) {
+    this.profile = profile;
+    if (profile != null) {
+      profile.setUser(this);
     }
+  }
 
-    public long getId() {
-        return id;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Profile getProfile() {
-        return profile;
-    }
-
-    public void setProfile(Profile profile) {
-        this.profile = profile;
-        if (profile != null) {
-            profile.setUser(this);
-        }
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
